76c085d6b9d6bce26a867881d98ce1bc57383d13
Jose Boza <jaboza@gmail.com>
2014-08-12 11:41:00 -0600

    dont do batch requests for empty arrays

--- lib/gmail_api/label.rb
+++ lib/gmail_api/label.rb
@@ -5,10 +5,16 @@ module GmailApi
     def self.find_collection(client, ids)
       calls = ids.reject { |id| id =~ /INBOX|CATEGORY_|STARRED|SENT/ }.map { |id| { api_method: GmailApi.api.users.labels.get, parameters: { id: id, 'userId'=>'me' }  } }
       labels = []
-      client.execute_batch(calls) do |result|
-        labels << Label.new(result)
-      end
+      get_batched_labels(labels)
       labels
+    end
+
+    def self.get_batched_labels(labels)
+      if labels.any?
+        client.execute_batch(calls) do |result|
+          labels << Label.new(result)
+        end
+      end
     end
       
     def self.find(client, id)
